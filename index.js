b72e56b807a0c67d8273c20d0a1b0f4dec938290
Chris Chu <cschu85@gmail.com>
2018-10-03 16:23:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+// shout
+function shout(string){
+  return string.toUpperCase()
+}
+// whisper
+function whisper(string){
+  return string.toLowerCase()
+}
+// logshout
+function logShout(string){
+  var spy = string.toUpperCase
+  console.log(spy)
+}
